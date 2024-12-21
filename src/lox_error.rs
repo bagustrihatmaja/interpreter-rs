8f07c09b270c9e184ac8954310724ca6a138294e
Bagus Trihatmaja <astungkara.project@gmail.com>
2024-12-20 20:55:31 -0500

    scanner

---
+++ src/lox_error.rs
@@ -0,0 +1,54 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub enum LoxError {
+    LexicalError(Error),
+    UnexpectedError(Error),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Error {
+    line: usize,
+    error_where: String,
+    message: String,
+}
+
+impl Error {
+    pub fn new(line: usize, error_where: String, message: String) -> Self {
+        Self {
+            line,
+            error_where,
+            message,
+        }
+    }
+
+    pub fn error(line: usize, message: String) -> Self {
+        Self {
+            line,
+            message,
+            error_where: "".into(),
+        }
+    }
+
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "[line {}] Error {}: {}",
+            self.line, self.error_where, self.message
+        )
+    }
+}
+
+
+impl fmt::Display for LoxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let lox_error_message = match self {
+            LoxError::LexicalError(error) => format!("{error}"),
+            LoxError::UnexpectedError(error) => format!("{error}"),
+        };
+        write!(f, "{}", lox_error_message)
+    }
+}
